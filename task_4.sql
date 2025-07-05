3c0f96079437964ea01f02a9c64b9df3a2d29a29
Mzamil Bundid <mzamil.bundid@gmail.com>
2025-07-05 21:01:51 +0300

    Fix task_4.sql to show columns of books table without DESCRIBE or ANALYZE

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,4 @@
-USE alx_book_store;
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books'
-ORDER BY ORDINAL_POSITION;
+-- task_4.sql
+-- Show full description of the books table in alx_book_store database
+
+SHOW COLUMNS FROM books;
