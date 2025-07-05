4f4480b8e8743934cef2bbc7fa04b217f10e4f84
Mzamil Bundid <mzamil.bundid@gmail.com>
2025-07-05 21:09:28 +0300

    Delete task_4.sql

--- task_4.sql
+++
@@ -1,6 +0,0 @@
--- task_4.sql
--- Show full description of the books table in alx_book_store database
-
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
