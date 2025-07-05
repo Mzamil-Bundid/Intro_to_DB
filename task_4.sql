5c3190ed3191b5620aaf5be5bb9a3eefed9e9aa7
Mzamil Bundid <mzamil.bundid@gmail.com>
2025-07-05 21:07:22 +0300

    Update task_4.sql to use INFORMATION_SCHEMA for books table description

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,6 @@
 -- task_4.sql
 -- Show full description of the books table in alx_book_store database
 
-SHOW COLUMNS FROM books;
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
